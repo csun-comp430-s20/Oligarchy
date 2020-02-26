f7938ee6dec9f36fafff37fac1b8903b0992cb4a
jiaminzhu327 <38767870+jiaminzhu327@users.noreply.github.com>
2020-02-25 19:36:41 -0800

    Jiamin's Token

--- src/Lexer.scala
+++ src/Lexer.scala
@@ -1,3 +1,10 @@
 class Lexer {
-
+  
 }
+
+case object FuncToken extends Token{}
+case object  SubtractToken extends Token{}
+case object LessThanToken extends Token{}
+case object LeftCurlyToken extends Token{}
+case object  ForToken extends Token{}
+case object ConstructorToken extends Token{}
