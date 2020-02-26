aeb780aa5b1bcc52d6fb90ab7505e15f7ab60a4f
Imon Daneshmand <imondaneshmand@gmail.com>
2020-02-25 19:27:38 -0800

    Add: StrToken, MultiplicationToken, GreaterThanToken, RightCurlyToken, RightParenToken, PrintParenToken

--- src/Lexer.scala
+++ src/Lexer.scala
@@ -1,3 +1,12 @@
+sealed trait Token
+
+case class StrToken(value: String) extends Token
+case object MultiplicationToken extends Token
+case object GreaterThanToken extends Token
+case object RightCurlyToken extends Token
+case object RightParenToken extends Token
+case object PrintParenToken extends Token
+
 class Lexer {
 
 }
