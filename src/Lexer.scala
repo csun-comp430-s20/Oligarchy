b5dbd621634b198dd235a60ba718226704446f33
Stephanie <Stephanie.contreras.477@my.csun.edu>
2020-02-25 19:22:12 -0800

    Added Tokens for Var, Boolean, Caret, Equals, Left Paren, and Class

--- src/Lexer.scala
+++ src/Lexer.scala
@@ -1,3 +1,12 @@
+sealed trait Token
+
+case class VarToken(name: String) extends Token
+case class BooleanToken(name: Boolean) extends Token
+case object CaretToken extends Token
+case object EqualsToken extends Token
+case object LeftParenToken extends Token
+case object ClassToken extends Token
+
 class Lexer {
 
 }
