1dcd92a924cfebdcf362d4b42cbd529138373c8c
edheadonfire <eduardo.preciado.394@my.csun.edu>
2020-02-25 19:18:50 -0800

    Added my 6 token cases classname , plus, greaterthan, right curly, right paren, print

--- src/Lexer.scala
+++ src/Lexer.scala
@@ -1,3 +1,10 @@
+sealed trait Token
+case class ClassNameToken(name: String) extends Token
+case object PlusToken extends Token
+case object GreaterThanToken extends Token
+case object RightCurlyToken extends Token
+case object RightParenToken extends Token
+case object PrintToken extends Token
 class Lexer {
 
 }
