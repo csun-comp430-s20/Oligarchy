6cb4f69520d37d25f32bba5e08cff6d7b11d7d56
ddcardenas <47076789+ddcardenas@users.noreply.github.com>
2020-02-25 19:27:07 -0800

    Created Token classes/objects for tokens 5,10,15,20,25,30

--- src/Lexer.scala
+++ src/Lexer.scala
@@ -1,4 +1,12 @@
+sealed trait Token
+
+case class IntegerToken(value: Int) extends Token
+case object DivisionToken extends Token
+case object OrToken extends Token
+case object SemicolonToken extends Token
+case object ElseToken extends Token
+case object PeriodToken extends Token
+
 class Lexer {
   //this is stest to see if push is sending to daniel branch
-
 }
