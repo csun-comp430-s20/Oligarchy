f128f6b63043f7aed5812b6e2019f554f2f15a00
edheadonfire <eduardo.preciado.394@my.csun.edu>
2020-03-21 13:25:53 -0700

    made new class be able to have multiple exp

--- src/Parser.scala
+++ src/Parser.scala
@@ -28,7 +28,7 @@ case class LogicExp(e1:Exp , l1: Logic, e2: Exp) extends Exp
 case class MathExp(e1:Exp , m1: MathOp, e2: Exp) extends Exp
 case class PrintExp(e1:Exp) extends Exp
 case class MethodExp(e1:Exp , methodName: Variable, e2: Exp) extends Exp
-case class NewClassExp(className: Variable, e1:Exp ) extends Exp
+case class NewClassExp(className: Variable, e1:Exp* ) extends Exp
 case class CastExp(t1: Types , e2: Exp) extends Exp
 case class HighOrderExp(t1: Types , v1: Variable, e2: Exp) extends Exp
 case class CallHighOrderExp(e1:Exp , e2: Exp) extends Exp
