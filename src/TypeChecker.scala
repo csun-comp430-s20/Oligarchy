3396f29d9ac191cff9bd9ebb0378dbbf30c52d97
edheadonfire <eduardo.preciado.394@my.csun.edu>
2020-04-16 19:01:08 -0700

    typechecker first commit

---
+++ src/TypeChecker.scala
@@ -0,0 +1,17 @@
+case class IllTypedException(msg: String) extends Exception(msg)
+
+object Typechecker {
+  type TypeEnv = Map[Variable, Type]
+
+  def typeof(e: Exp, gamma: TypeEnv): Type = {
+
+  } // typeof
+
+  def typecheckStatement(s: Stmt, gamma: TypeEnv): TypeEnv = {
+
+  } // typecheckStatement
+
+  def typecheckProgram(input: Program, gamma: TypeEnv): TypeEnv = {
+
+  } // typecheckProgram
+} // Typechecker
---
+++ src/TypeCheckerTest.scala
@@ -0,0 +1,3 @@
+class TypeCheckerTest {
+
+}
