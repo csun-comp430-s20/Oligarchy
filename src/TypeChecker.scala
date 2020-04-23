b81fede303bc2ca850aede473933dacfc31a8573
Imon Daneshmand <imondaneshmand@gmail.com>
2020-04-22 22:06:07 -0700

    Merged changes into TypeChecker.scala

--- src/TypeChecker.scala
+++ src/TypeChecker.scala
@@ -5,7 +5,53 @@ object Typechecker {
   type TypeEnv = Map[, Type]
 
   def typeof(e: Exp, gamma: TypeEnv): Type = {
-
+    e match{
+      case VariableExp(x) if gamma.contains(x) => gamma(x)
+      case IntegerExp(_) => IntTypes
+      case BooleanExp(_) => BoolTypes
+      case AndExp(e1, e2) => {
+        (typeof(e1, gamma), typeof(e2, gamma)) match {
+          case (BoolTypes, BoolTypes) => BoolTypes
+          case _ => throw IllTypedException("and")
+        }
+      }
+      case PlusExp(e1, e2) => {
+        (typeof(e1, gamma), typeof(e2, gamma)) match {
+          case (IntTypes, IntTypes) => IntTypes
+          case _ => throw IllTypedException("add")
+        }
+      }
+      case OrExp(e1, e2) => {
+        (typeof(e1, gamma), typeof(e2, gamma)) match {
+          case (BoolTypes, BoolTypes) => BoolTypes
+          case _ => throw IllTypedException("or")
+        }
+      }
+      case GTExp(e1, e2) => {
+        (typeof(e1, gamma), typeof(e2, gamma)) match {
+          case (IntTypes, IntTypes) => BoolTypes
+          case _ => throw IllTypedException("greater than")
+        }
+      }
+      case GTEExp(e1, e2) => {
+        (typeof(e1, gamma), typeof(e2, gamma)) match {
+          case (IntTypes, IntTypes) => BoolTypes
+          case _ => throw IllTypedException("greater than equals")
+        }
+      }
+      case LTExp(e1, e2) => {
+        (typeof(e1, gamma), typeof(e2, gamma)) match {
+          case (IntTypes, IntTypes) => BoolTypes
+          case _ => throw IllTypedException("less than")
+        }
+      }
+      case LTEExp(e1, e2) => {
+        (typeof(e1, gamma), typeof(e2, gamma)) match {
+          case (IntTypes, IntTypes) => BoolTypes
+          case _ => throw IllTypedException("less than equals")
+        }
+      }
+    } // match
   } // typeof
 
   def typecheckStatement(s: Stmt, gamma: TypeEnv): TypeEnv = {
