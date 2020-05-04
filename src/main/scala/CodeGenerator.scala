e69c53fa7d3fbdabda4ef05b9fb9d6f0468d79a9
edheadonfire <eduardo.preciado.394@my.csun.edu>
2020-05-03 22:41:47 -0700

    added some basic test that we will need to have filled by the time we are done

--- src/main/scala/CodeGenerator.scala
+++ src/main/scala/CodeGenerator.scala
@@ -1,8 +1,47 @@
-import org.objectweb.asm.MethodVisitor
-import org.objectweb.asm.Opcodes.ILOAD
-import org.objectweb.asm.Opcodes.ISTORE
+import java.sql.Statement
 
+import org.objectweb.asm.MethodVisitor
+import org.objectweb.asm.ClassWriter
 class CodeGeneratorException(val message: String) extends Exception(message)
-class CodeGenerator {
+class CodeGenerator(
+                     val outputClassName: String,
+                     val outputMethodName: String,
+                     classWriter: ClassWriter,
+                     variables: Map[String,VariableEntry],
+                     nextIndex: Int,
+                     methodVisitor: MethodVisitor
+                   ) {
+
+
+  // should be similar to the constructor method in java here we will set up how we will generate code
+  def apply(outputClassName: String, outputMethodName: String): CodeGenerator = {
+  }//apply
+
+  private def getEntryFor(variable: String): VariableEntry = {
+  } // getEntryFor
+
+  private def addEntry(variable: String, types: Types): VariableEntry = if (variables contains variable) { // should be caught by typechecker
+  }// addEntry
+
+  private def writeIntLiteral(value: Int): Unit = {
+  }// writeIntLiteral
+  private def writeOp(binaryOp: Nothing): Unit ={
+  }// writeOp
+  private def writeExpression(exp: Nothing): Unit={
+  }// writeExpression
+  private def writePrint(variable:String): Unit={
+  }//writePrint
+  private def writeStatement(statement: Nothing):Unit={
+  }//writeStatement
+  private def writeVarDeclaration(varDec:  Nothing):Unit={
+  }//writeVarDeclaration
+  private def writeInstance(instance:  Nothing):Unit={
+  }//writeInstance
+  private def writeMethod(method:  Nothing):Unit={
+  }//writeMethod
+  private def writeClass(myClass:  Nothing):Unit={
+  }//writeClass
+  private def writeProgram(program: Nothing):Unit={
+  }//writeProgram
 
 }
---
+++ src/test/scala/CodeGeneratorTest.scala
@@ -0,0 +1,22 @@
+import java.io.File
+
+import org.scalatest.funsuite.AnyFunSuite
+
+
+
+class CodeGeneratorTest extends AnyFunSuite {
+
+
+  test("Print the number 1")
+  test("Print the number 2") // so on this is basic
+  // printing of integers we can then skip to largest of int
+  test("Print a true value")
+  test("Print a false value")
+  test("try an Integer assignment")// try every kind of assign
+  // we have
+  test("try addition") // try every math operation
+  // its ok if we need to remove some
+  test("try a math operation on variables")
+
+
+}
