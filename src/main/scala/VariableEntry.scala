ab2c81f2af13e501ff0124d3c534949a9b812f81
edheadonfire <eduardo.preciado.394@my.csun.edu>
2020-05-11 15:36:06 -0700

    added static methos for variable entry

--- src/main/scala/VariableEntry.scala
+++ src/main/scala/VariableEntry.scala
@@ -1,6 +1,28 @@
 import org.objectweb.asm.MethodVisitor
-import org.objectweb.asm.Opcodes.ILOAD
-import org.objectweb.asm.Opcodes.ISTORE
+import org.objectweb.asm.Opcodes._
+
+case object VariableEntry {
+  @throws[CodeGeneratorException]
+  def loadInstructionForType(types: Types): Int = { // both are treated as integers at the bytecode level
+    types match {
+      case IntTypes | BoolTypes => ILOAD
+      case ClassTypes(className) =>  ALOAD
+      case MethodTypes(paramTypes, returnTypes) => ALOAD
+      case _ => throw new CodeGeneratorException("Unknown load type: " + types)
+    }
+  } // loadInstructionForType
+
+
+  @throws[CodeGeneratorException]
+  def storeInstructionForType(types: Types) = {
+    types match {
+      case IntTypes | BoolTypes => ISTORE
+      case ClassTypes(className) => ASTORE
+      case MethodTypes(paramTypes, returnTypes) => ASTORE
+      case _ => throw new CodeGeneratorException("Unknown storetype: " + types)
+    }
+  } // storeInstructionForType
+}
 
 class VariableEntry(val variable: String, val types: Types, val index: Int) {
   assert(index >= 0)
@@ -18,5 +40,6 @@ class VariableEntry(val variable: String, val types: Types, val index: Int) {
         visitor.visitVarInsn(ISTORE, index)
     }
   }
+
   // VariableEntr
 }
