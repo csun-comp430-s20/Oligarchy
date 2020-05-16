16d1eb7c811da9810934135af4885dfbd2e526f7
edheadonfire <eduardo.preciado.394@my.csun.edu>
2020-05-16 15:53:34 -0700

    added some test files

--- src/main/scala/compiler.scala
+++ src/main/scala/compiler.scala
@@ -2,15 +2,17 @@ import scala.io.Source
 
 object compiler {
   def main(args: Array[String]) {
-    val filename = "/Users/stephanie/Documents/Comp430/Oligarchy/src/main/scala/test.txt"
-    val input = Source.fromFile(filename).getLines().mkString
-
+//    val filename = "C:\\Users\\edpre\\JavaProjects\\Oligarchy\\src\\main\\scala\\createExtendedClass.txt"
+    val filename = "C:\\Users\\edpre\\JavaProjects\\Oligarchy\\src\\main\\scala\\createAndCallHighOrderFunction.txt"
+    val source = Source.fromFile(filename)
+    val input = source.getLines().mkString
+    source.close()
     val tokenizer = Lexer(input)
     val tokenizerOutput:List[Token] = tokenizer.tokenize()
     val parser = Parser(tokenizerOutput)
     val parserOutput = parser.parseProgram(tokenizerOutput)
     val typechecker = Typechecker(parserOutput._1)
     val codeGen = ClassGenerator(parserOutput._1)
-    codeGen.writeClasses("/Users/stephanie/Documents/Comp430/Oligarchy/src/main/scala/")
+    codeGen.writeClasses("C:\\Users\\edpre\\JavaProjects\\Oligarchy\\src\\main\\scala\\")
   }
 }
--- src/main/scala/main.java
+++ src/main/scala/main.java
@@ -1,8 +1,11 @@
 
 public class main {
     public static void main(String args[]){
-//        Testing x = new Testing();
-//        int i = x.testMethod(1);
-//        System.out.println(i);
+//        Child x = new Child();
+//        int a = x.childMethod(1);
+//        Parent y = new Parent();
+//        int b = y.parentMethod(1);
+//        System.out.println(a);
+//        System.out.println(b);
     }
 }
